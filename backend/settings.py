db091ea5eef1d25b636822a1d18e3853a7acebed
Alexey Panaetov <panaetovaa@gmail.com>
2025-12-22 16:30:27 +0500

    oooo

---
+++ backend/settings.py
@@ -0,0 +1,18 @@
+import os
+
+DATABASE_HOST = os.environ.get("SERVICE_DATABASE_HOST", "db")
+DATABASE_NAME = os.environ.get("SERVICE_DATABASE_NAME", "dnd")
+DATABASE_PORT = int(os.environ.get("SERVICE_DATABASE_PORT", "5437"))
+
+DATABASE_DSN = (
+    f"postgresql+asyncpg://dude:dude_password_123@"
+    f"{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+)
+
+DB_CONFIG = {
+    "host": DATABASE_HOST,
+    "port": DATABASE_PORT,
+    "user": "dude",
+    "password": "dude_password_123",
+    "database": DATABASE_NAME,
+}
