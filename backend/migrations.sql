94a4ea613c178b5143ea2a7b04a1171c5728edda
Alexey Panaetov <panaetovaa@gmail.com>
2025-12-23 13:25:55 +0500

    iiii

--- backend/migrations.sql
+++ backend/migrations.sql
@@ -38,9 +38,24 @@ CREATE TABLE IF NOT EXISTS characters (
 );
 
 
+CREATE TABLE IF NOT EXISTS maps (
+    id serial PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+
+    game_id int UNIQUE NOT NULL REFERENCES games(id),
+    url TEXT NOT NULL,
+    x_center integer DEFAULT 50,
+    y_center integer DEFAULT 50,
+    zoom integer DEFAULT 1
+);
+
+
 insert into masters (external_id) values ('alexey');
 
-insert into games (external_id, master_id, master_join_link) values ('123', 1, 'qwe');
+insert into games (external_id, name, master_id, master_join_link) values ('123', 'Game #1', 1, 'qwe');
 
 insert into characters (external_id, name, game_id, join_link, avatar_url, race) values
-('player-1', 'Arnold', 1, 'rty', 'https://google.com', 'elf');
+('player-1', 'Arnold', 1, 'rty', 'https://storage.yandexcloud.net/dnd2/player1.jpg', 'elf');
+
+insert into maps (game_id, url) values (1, 'https://storage.yandexcloud.net/dnd2/map.png');
