52c41beb46c1d4e709632bf4bd0075b44fa9f5eb
Alexey Panaetov <panaetovaa@gmail.com>
2025-12-26 23:12:21 +0500

    iii

--- backend/server.py
+++ backend/server.py
@@ -3,8 +3,9 @@ import logging
 from contextlib import asynccontextmanager
 from typing import Dict, List
 
-from fastapi import FastAPI, Response, WebSocket, WebSocketDisconnect
-from fastapi.responses import RedirectResponse
+import pydantic
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 
 import database
 
@@ -29,48 +30,63 @@ async def lifespan(*_, **__):
 app = FastAPI(lifespan=lifespan)
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class JoinResponse(pydantic.BaseModel):
+    game_id: str
+    user_id: str = ""
+    master_id: str = ""
+
+
 @app.get("/join-as-master/{link}")
-async def join_as_master_handler(link: str, response: Response):
+async def join_as_master_handler(link: str) -> JoinResponse:
     game = await database.Game.find_by_master_link(link)
     assert game
 
     master = await database.Master.find_by_id(game.master_id)
     assert master
 
-    response = RedirectResponse(f"/{game.external_id}")
-
-    response.set_cookie(key="game_id", value=game.external_id)
-    response.set_cookie(key="user_id", value=f"m:{master.external_id}")
-    return response
+    return JoinResponse(
+        game_id=game.external_id,
+        master_id=master.external_id,
+    )
 
 
 @app.get("/join/{link}")
-async def join_as_character_handler(link: str, response: Response):
+async def join_as_character_handler(link: str) -> JoinResponse:
     cha = await database.Character.find_by_join_link(link)
     assert cha
 
     game = await database.Game.find_by_id(cha.game_id)
     assert game
 
-    response = RedirectResponse(f"/{game.external_id}")
-
-    response.set_cookie(key="game_id", value=game.external_id)
-    response.set_cookie(key="user_id", value=f"c:{cha.external_id}")
-    return response
+    return JoinResponse(
+        game_id=game.external_id,
+        user_id=cha.external_id,
+    )
 
 
 @app.get("/game/{game_external_id}/map")
-async def get_map_handler(game_external_id: str):
+async def get_map_handler(game_external_id: str) -> database.Map:
     gmap = await database.Map.find_by_game_external_id(game_external_id)
-    return gmap.model_dump()
+    return gmap
 
 
 @app.get("/game/{game_external_id}/character/{character_external_id}")
-async def get_character_handler(game_external_id: str, character_external_id: str):
+async def get_character_handler(
+    game_external_id: str, character_external_id: str
+) -> database.Character:
     logger.info(f"Game = {game_external_id}")
     cha = await database.Character.find_by_external_id(character_external_id)
 
-    return cha.model_dump()
+    return cha
 
 
 ACTIVE_CONNECTIONS: Dict[str, List[WebSocket]] = {}
